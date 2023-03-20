53fa6c3dd00e9346efcfbea43b47a6593ae171b9
LosEblos <128365827+LosEblos@users.noreply.github.com>
2023-03-20 15:57:21 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# JSDays23-Mo-Angular2-11-Routing
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-uelwhb-91wqvy)
---
+++ src/app/components/todo-edit/todo-edit.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { TodoService } from '../../services/todo.service';
+import { Todo } from '../../models/todo';
+import { Observable } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-todo-edit',
+  templateUrl: './todo-edit.component.html',
+  styleUrls: ['./todo-edit.component.css'],
+})
+export class TodoEditComponent implements OnInit {
+  public todo$: Observable<Todo>;
+
+  constructor(
+    private readonly activatedRoute: ActivatedRoute,
+    private readonly todoService: TodoService
+  ) {}
+
+  ngOnInit() {
+    this.todo$ = this.activatedRoute.params.pipe(
+      map((params) => params?.['id']),
+      switchMap((id) => {
+        return this.todoService.get(id);
+      })
+    );
+  }
+}
